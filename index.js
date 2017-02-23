06b5c2d9ef4739543cad855d7a5bcaa8dad83d50
Ekaterina Petushova <epetushova@gmail.com>
2017-02-23 16:24:46 -0500

    Done.

--- index.js
+++ index.js
@@ -10,13 +10,13 @@ function Cat(name, breed, age){
 	this.name = name;
 	this.breed = breed;
 	this.age = age;
-}
+	}
 
 function Ferret(name, age, foodRequirements){
 	this.name = name;
 	this.age = age;
 	this.foodRequirements = foodRequirements;
-}
+	}
 
 function Bird(name, breed){
 	this.name = name;
